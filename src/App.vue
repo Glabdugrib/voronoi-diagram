2343cf0ebecdb34c04f2149b55c8979f4e2f0653
Simone Sada <si.sada93@gmail.com>
2026-06-24 18:30:44 +0200

    refactor: extract VoronoiCanvas component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useCanvas } from "@/composables/useCanvas";
-
-const canvasRef = ref<HTMLCanvasElement | null>(null);
-
-useCanvas(canvasRef);
+import VoronoiCanvas from "./components/VoronoiCanvas.vue";
 </script>
 
 <template>
-  <canvas ref="canvasRef" class="block h-screen w-screen border-4 border-indigo-500"></canvas>
+  <VoronoiCanvas />
 </template>
---
+++ src/components/VoronoiCanvas.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useCanvas } from "../composables/useCanvas";
+
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+
+useCanvas(canvasRef);
+</script>
+
+<template>
+  <canvas ref="canvasRef" class="block h-screen w-screen border-4 border-indigo-500"></canvas>
+</template>
